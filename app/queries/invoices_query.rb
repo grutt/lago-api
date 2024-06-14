31b3f1e206f2a8fb05ea8596cf90686d32c9752d
Ancor Cruz <hello@ancorcruz.com>
2024-06-14 13:12:07 +0100

    move filters to the filters hash in InvoicesQuery
    `#call` expects filters within the filters hash,
    `search_term` remains as search instead of filter

--- app/queries/invoices_query.rb
+++ app/queries/invoices_query.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class InvoicesQuery < BaseQuery
-  def call(search_term:, status:, page:, limit:, filters: {}, customer_id: nil, payment_status: nil, payment_dispute_lost: nil) # rubocop:disable Metrics/ParameterLists
+  def call(search_term:, page:, limit:, filters: {})
     @search_term = search_term
-    @customer_id = customer_id
+    @customer_id = filters[:customer_id]
 
     invoices = base_scope.result.includes(:customer)
     invoices = invoices.where(id: filters[:ids]) if filters[:ids].present?
-    invoices = invoices.where(customer_id:) if customer_id.present?
-    invoices = invoices.where(status:) if status.present?
-    invoices = invoices.where(payment_status:) if payment_status.present?
-    invoices = invoices.where.not(payment_dispute_lost_at: nil) unless payment_dispute_lost.nil?
+    invoices = invoices.where(customer_id: filters[:customer_id]) if filters[:customer_id].present?
+    invoices = invoices.where(status: filters[:status]) if filters[:status].present?
+    invoices = invoices.where(payment_status: filters[:payment_status]) if filters[:payment_status].present?
+    invoices = invoices.where.not(payment_dispute_lost_at: nil) if filters[:payment_dispute_lost]
     invoices = invoices.order(issuing_date: :desc, created_at: :desc).page(page).per(limit)
 
     result.invoices = invoices
